e5f28be63820f84897799f12919ab66b2f4f66b4
Pulkit Bhuwalka <pulkit.bosco05@gmail.com>
2014-05-14 20:40:33 -0400

    Setting up schema in mysql.

--- src/main/resources/sql/load_schema.sql
+++ src/main/resources/sql/load_schema.sql
@@ -9,13 +9,18 @@ grant all privileges on maori.* to 'maori'@'localhost';
 use maori;
 
 create table model (
-    id int,
-    name text,
-    model longblob
+    id int not null auto_increment primary key,
+    name varchar(255) not null,
+    version varchar(255) not null,
+    active bit,
+    archive bit,
+    payload longblob not null
 );
 
-create table model_associations (
-    id int,
-    model_id int,
-    client_id char(30)
+-- TODO: Constraint name+version unique
+
+create table model_devices (
+    id int not null auto_increment primary key,
+    model_id int not null,
+    device_id char(30) not null
 );
