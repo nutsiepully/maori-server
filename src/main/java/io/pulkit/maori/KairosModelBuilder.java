79e332dd46022e8518b146607ae6ae1a32600c15
Daniel <danielsilva.ita@gmail.com>
2014-12-07 02:33:56 -0500

    Kairos data + random forest model

--- src/main/java/io/pulkit/maori/KairosModelBuilder.java
+++ src/main/java/io/pulkit/maori/KairosModelBuilder.java
@@ -4,6 +4,7 @@ import io.pulkit.maori.controllers.KairosController;
 import weka.classifiers.bayes.NaiveBayesUpdateable;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
+import weka.core.converters.ArffLoader;
 import weka.core.converters.CSVLoader;
 
 import java.io.File;
@@ -14,19 +15,34 @@ import java.net.URL;
 
 public class KairosModelBuilder {
     public static void main(String[] args) throws Exception {
-        URL file = Thread.currentThread().getContextClassLoader().getResource("kairos/data_1416450449843_v2.txt");
-
-        CSVLoader loader = new CSVLoader();
+        URL file = Thread.currentThread().getContextClassLoader().getResource("kairos/d5_withHeader.arff");
+        String outputFolder = "src/main/resources/kairos/";
+        
+        //CSVLoader loader = new CSVLoader();
+        ArffLoader loader = new ArffLoader();
+        
+        //loader.setOptions(new String[]{"-H"});//if file has no header
+        
         loader.setSource(new File(file.getPath()));
         Instances kairosData = loader.getDataSet();
-        kairosData.setClassIndex(9);
+        kairosData.setClassIndex(0);
 
+        //-I <number of trees> 
+        //-K <number of features>
         RandomForest randomForest = new RandomForest();
+        randomForest.setOptions(new String[]{"-I","100","-K","5"});
         randomForest.buildClassifier(kairosData);
 
-        FileOutputStream fileOutputStream = new FileOutputStream(KairosController.KAIROS_MODEL_RANDOM_FOREST);
+        //output model
+        FileOutputStream fileOutputStream = new FileOutputStream(outputFolder+KairosController.KAIROS_MODEL_RANDOM_FOREST);
         ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
         outputStream.writeObject(randomForest);
+        outputStream.close();
+        fileOutputStream.close();
+        //output instances
+        fileOutputStream = new FileOutputStream(outputFolder+KairosController.KAIROS_INSTANCES_RANDOM_FOREST);
+        outputStream = new ObjectOutputStream(fileOutputStream);
+        outputStream.writeObject(kairosData);
         outputStream.close();
         fileOutputStream.close();
     }
--- src/main/java/io/pulkit/maori/controllers/KairosController.java
+++ src/main/java/io/pulkit/maori/controllers/KairosController.java
@@ -5,41 +5,75 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-import weka.classifiers.bayes.NaiveBayesUpdateable;
 import weka.classifiers.trees.RandomForest;
 import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping(value = "/kairos")
 public class KairosController {
     public static final String KAIROS_MODEL_RANDOM_FOREST = "kairos-model-random-forest";
+    public static final String KAIROS_INSTANCES_RANDOM_FOREST = "kairos-instances-random-forest";
+    public static final String KAIROS_RESOURCE_FOLDER = "kairos";
+    public static final int N_FEATURES = 16; //includes label
+    public static final HashMap<Double, String> labelMap = new HashMap<Double, String>(); 
+    
 
     private RandomForest randomForest;
+    private Instances dataset;
 
     private void initModel() throws IOException, ClassNotFoundException {
-        URL file = Thread.currentThread().getContextClassLoader().getResource(KAIROS_MODEL_RANDOM_FOREST);
+        URL file = Thread.currentThread().getContextClassLoader().getResource(KAIROS_RESOURCE_FOLDER+"/"+KAIROS_MODEL_RANDOM_FOREST);
         FileInputStream fis = new FileInputStream(file.getPath());
         ObjectInputStream ois = new ObjectInputStream(fis);
         randomForest = (RandomForest)ois.readObject();
+        ois.close();
+    }
+    
+    private void initInstances() throws IOException, ClassNotFoundException {
+        URL file = Thread.currentThread().getContextClassLoader().getResource(KAIROS_RESOURCE_FOLDER+"/"+KAIROS_INSTANCES_RANDOM_FOREST);
+        FileInputStream fis = new FileInputStream(file.getPath());
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        dataset = (Instances)ois.readObject();
+        ois.close();
+        
+        Enumeration<Object> en = dataset.attribute(0).enumerateValues();
+        int i=0;
+        while(en.hasMoreElements()){
+        	labelMap.put((double)i++, en.nextElement().toString());
+        }
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/predict")
     @ResponseBody
     public String activateModel(@RequestParam String inputFeatures) throws Exception {
+    	
         if (randomForest == null) initModel();
+        if (dataset == null) initInstances();
 
         String[] features = inputFeatures.split(",");
-
-        double result = randomForest.classifyInstance(new DenseInstance(5));
-
-        throw new NotImplementedException();
+        double[] dFeatures = new double[N_FEATURES];
+        if(features.length!=N_FEATURES-1){
+        	throw new IllegalArgumentException();
+        }
+        for(int i=1;i<N_FEATURES;i++){
+        	dFeatures[i] = Double.parseDouble(features[i-1]);
+        }
+        
+        Instance instance = new DenseInstance(1, dFeatures);
+        instance.setDataset(dataset);
+        
+        double result_idx = randomForest.classifyInstance(instance);
+        String result = labelMap.get(result_idx);
+        return result;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/hello")
