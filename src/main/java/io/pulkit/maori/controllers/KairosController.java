17b8dd582e989175bcc5fe9c227a324aff909b43
Daniel <danielsilva.ita@gmail.com>
2014-12-08 09:36:19 -0500

    label adjustment

--- src/main/java/io/pulkit/maori/controllers/KairosController.java
+++ src/main/java/io/pulkit/maori/controllers/KairosController.java
@@ -10,8 +10,11 @@ import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.net.URL;
 import java.util.Enumeration;
@@ -20,12 +23,18 @@ import java.util.HashMap;
 @Controller
 @RequestMapping(value = "/kairos")
 public class KairosController {
-    public static final String KAIROS_MODEL_RANDOM_FOREST = "kairos-model-random-forest";
+	public static final String KAIROS_RESOURCE_FOLDER = "kairos";
+	public static final String KAIROS_MODEL_RANDOM_FOREST = "kairos-model-random-forest";
     public static final String KAIROS_INSTANCES_RANDOM_FOREST = "kairos-instances-random-forest";
-    public static final String KAIROS_RESOURCE_FOLDER = "kairos";
-    public static final int N_FEATURES = 16; //includes label
-    public static final HashMap<Double, String> labelMap = new HashMap<Double, String>(); 
+    public static final String KAIROS_LABEL_NAMES = "labelNames.csv";
     
+    public static final int N_FEATURES = 16; //includes label
+    //labels are saved as numbers, but they are not in the right order in the dataset. 
+    // labelMap maps the index of the label to the label number
+    // e.g. 0->"8" means that class 8 occupies position 0
+    public static final HashMap<Double, String> labelMap = new HashMap<Double, String>();
+    //labelNames maps the label number to its name. E.g. "8" -> "STOPPED"
+    public static final HashMap<String, String> labelNames = new HashMap<String, String>();
 
     private RandomForest randomForest;
     private Instances dataset;
@@ -45,11 +54,27 @@ public class KairosController {
         dataset = (Instances)ois.readObject();
         ois.close();
         
+        initLabelNames();
+        
         Enumeration<Object> en = dataset.attribute(0).enumerateValues();
         int i=0;
         while(en.hasMoreElements()){
-        	labelMap.put((double)i++, en.nextElement().toString());
+        	labelMap.put((double)i++, labelNames.get(en.nextElement().toString()));
         }
+    }
+    
+    private void initLabelNames() throws IOException{
+    	URL file = Thread.currentThread().getContextClassLoader().getResource(KAIROS_RESOURCE_FOLDER+"/"+KAIROS_LABEL_NAMES);
+        FileInputStream fis = new FileInputStream(file.getPath());
+        BufferedReader reader = null;
+    	reader = new BufferedReader(new InputStreamReader(fis));
+        String line = reader.readLine();
+        while(line != null){
+            String[] valueAndName = line.split(",");
+            labelNames.put(valueAndName[0], valueAndName[1]);
+            line = reader.readLine();
+        }  
+        reader.close();
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/predict")
